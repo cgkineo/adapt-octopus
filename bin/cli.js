f0dd915d96367e5654e306b92f9de04b50ecb137
Thomas Taylor <thomas.taylor@kineo.com>
2022-06-30 13:01:53 +0100

    Add ability to recursively run for a framework src

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,39 @@
 #!/usr/bin/env node
+import fs from 'fs/promises';
+import path from 'path';
 import Octopus from "../lib/Octopus.js";
 
-const octopus = new Octopus({ inputPath: process.argv[2], inputId: process.argv[3] });
 
-octopus.start().catch(error => console.error(error));
+async function startRecursiveOctopus(arg) {
+  const cwd = path.resolve(arg);
+
+  Promise.all(['components', 'extensions', 'menu', 'theme'].map(async f => {
+    const rootDir = `${cwd}/src/${f}`;
+    const contents = await fs.readdir(rootDir);
+
+    return Promise.all(contents.map(async c => {
+      const pluginDir = `${rootDir}/${c}`;
+      const bowerJson = JSON.parse(await fs.readFile(`${pluginDir}/bower.json`));
+      const id = bowerJson.component || bowerJson.extension || bowerJson.menu || bowerJson.theme;
+      return startOctopus(id, pluginDir);
+    }));
+  }));
+}
+
+async function startOctopus(inputId, cwd, inputPath = 'properties.schema') {
+  try {
+    const octopus = new Octopus({ cwd, inputPath, inputId });
+    await octopus.start();
+  } catch(e) {
+    console.error('error', e);
+  }
+}
+
+async function run() {
+  process.argv.length === 4 ? 
+    startOctopus(process.argv[3], process.argv[2]) :
+    startRecursiveOctopus(process.argv[2]);
+}
+
+run();
+
