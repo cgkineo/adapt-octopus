5d58254710aebf1e7e2923b18ee011bf0f7d4186
Thomas Taylor <thomas.taylor@kineo.com>
2022-06-30 13:50:47 +0100

    Remove args

--- bin/cli.js
+++ bin/cli.js
@@ -4,8 +4,8 @@ import Octopus from "../lib/Octopus.js";
 
 async function run() {
   process.argv.length === 4 ?
-    Octopus.run({ inputPath: path.resolve(process.argv[2]), id: process.argv[3], isCli: true }) : 
-    Octopus.runRecursive({ cwd: path.resolve(process.argv[2]), isCli: true });
+    Octopus.run({ inputPath: path.resolve(process.argv[2]), id: process.argv[3] }) : 
+    Octopus.runRecursive({ cwd: path.resolve(process.argv[2]) });
 }
 
 run();
