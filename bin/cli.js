04e438b10d841946893693457ac2aa4e67ef9b4f
Thomas Taylor <thomas.taylor@kineo.com>
2023-01-05 12:48:20 +0000

    Fix CLI for individual plugins

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,10 @@ import path from 'path';
 import Octopus from "../lib/Octopus.js";
 
 async function run() {
+  const opts = { cwd: path.resolve(process.argv[2]), inputId: process.argv[3] };
   process.argv.length === 4 ?
-    Octopus.run({ inputPath: path.resolve(process.argv[2]), id: process.argv[3] }) : 
-    Octopus.runRecursive({ cwd: path.resolve(process.argv[2]) });
+    await Octopus.run(opts) :
+    await Octopus.runRecursive(opts);
 }
 
 run();
-
