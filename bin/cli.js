844b29b8147cab1f92176cf65171f744b39f4650
Oliver Foster <oliver.foster@kineo.com>
2023-03-16 11:55:02 +0000

    Update: Applied eslint standard

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import path from 'path';
-import Octopus from "../lib/Octopus.js";
+import path from 'path'
+import Octopus from '../lib/Octopus.js'
 
-async function run() {
-  const opts = { cwd: path.resolve(process.argv[2]), inputId: process.argv[3] };
-  process.argv.length === 4 ?
-    await Octopus.run(opts) :
-    await Octopus.runRecursive(opts);
+async function run () {
+  const opts = { cwd: path.resolve(process.argv[2]), inputId: process.argv[3] }
+  process.argv.length === 4
+    ? await Octopus.run(opts)
+    : await Octopus.runRecursive(opts)
 }
 
-run();
+run()
